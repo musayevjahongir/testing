3b8089e6b82fbe09586215f7cb11b51de34f972e
musayevjahongir <musayevjahongir5051@gmail.com>
2023-11-16 10:03:29 +0500

    max person

--- main.py
+++ main.py
@@ -1,32 +1,11 @@
 import json
-
-file_name = 'data.txt'
-
-def read_file(file_name: str) -> str:
-    f = open(file=file_name)
-    data = f.read()
-    f.close()
-    return data
-
-def to_dict(data: str) -> dict:
-    try:
-        numbers = json.loads(data)
-    except json.decoder.JSONDecodeError:
-        numbers = {
-            "numbers": []
-        }
-
-    return numbers
-
-def sum_of_numbers(numbers: dict) -> int:
-    s = 0
-    for number in numbers['numbers']:
-        s += number
-
-    return s
-
-data = read_file(file_name)
-numbers = to_dict(data)
-s = sum_of_numbers(numbers)
-
-print(s)
+from datetime import datetime 
+def read_file(file_name):
+    return open(file_name).read()
+def to_dic(read_f):
+    return json.loads(read_f)
+def max_person(dic_file):
+    return min(dic_file, key=lambda person: datetime.fromisoformat(person["birthday"]))
+d=read_file("data.json")
+data=to_dic(d)
+print(max_person(data))
